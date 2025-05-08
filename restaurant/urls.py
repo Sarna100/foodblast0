76c668e2ff9927faf861187e5b349f3bca77aeda
bilas08 <bilasjnr100002@gmail.com>
2025-05-09 00:38:54 +0600

    update

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,7 @@
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
@@ -10,17 +11,11 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
 
     path('order/success/', views.order_success, name='order_success'),
-
-    path('', views.home, name='home'),
-    path('order/', views.order_page, name='order_page'),
     path('place_order/<int:item_id>/', views.place_order, name='place_order'),
     path('menu/', views.menu_view, name='menu'),
     path('menu/order/<int:item_id>/', views.order_item, name='order_item'),
     path('register/', views.register_view, name='register'),
- # name="order" must be there
-
-path('order/<int:item_id>/', views.order_item, name='order_item'),
+path('order/<int:item_id>/', views.order_item, name='order'),  # name="order" must be there
 
 
 ]
-
